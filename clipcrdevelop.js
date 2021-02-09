df54e467ddf58a3dea2faaf8b119170c9bdaa562
JGomezC145 <35159777+JGomezC145@users.noreply.github.com>
2021-02-09 14:09:00 -0600

    codeupdate

---
+++ clipcrdevelop.js
@@ -0,0 +1,34 @@
+/* Linea de bases unificadas */
+var tisgPRO = function(a=1) {
+    var time=a;
+  
+    return {
+      sistemlk: function(codebreak) {
+        if (codebreak == '29154') {
+            return "El codigo fuente ha sido desbloqueado."
+        } else {
+            return "El codigo es incorrecto, verifica los datos."
+        }
+      },
+  
+      setUser: function(permission) {
+        if (permission == 'developer') {
+            localStorage.setItem('user', 'develop_')
+            location.reload()
+        } else if (permission == 'normal') {
+            localStorage.setItem('user', 'standard')
+            location.reload()
+        } else {
+            return "No se reconoce el tipo de usuario_"
+        }
+      },
+  
+      getUser: function() {
+        return localStorage.getItem('user');
+      },
+  
+      Zero: function() {
+        return "Zero"
+      }
+    }
+  }
